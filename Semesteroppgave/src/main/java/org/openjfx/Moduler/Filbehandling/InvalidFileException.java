0aa8d8b77cfec9b2984633325a12e23f2b281f0b
zuhuurmoe <56063635+zuhuurmoe@users.noreply.github.com>
2020-04-08 05:02:19 +0200

    Delete InvalidFileException.java

--- Semesteroppgave/src/main/java/org/openjfx/Moduler/Filbehandling/InvalidFileException.java
+++
@@ -1,7 +0,0 @@
-package org.openjfx.Moduler.Filbehandling;
-
-public class InvalidFileException extends Exception {
-    public InvalidFileException (String message){
-        super(message);
-    }
-}
