4bb7f1eefca9d785cc3597900712fa3b41c7c96a
zuhuurmoe <56063635+zuhuurmoe@users.noreply.github.com>
2020-04-08 05:05:36 +0200

    Delete InvalidDatamaskinException.java

--- Semesteroppgave/src/main/java/org/openjfx/Moduler/Exception/InvalidDatamaskinException.java
+++
@@ -1,7 +0,0 @@
-package org.openjfx.Moduler.Exception;
-
-public class InvalidDatamaskinException extends Exception  {
-    public InvalidDatamaskinException(String message){
-        super(message);
-    }
-}
